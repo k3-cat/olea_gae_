8f1f4f7c762d5146f3527a7eabcb45b94854aaea
Pix-00 <15878537+Pix-00@users.noreply.github.com>
2018-12-22 17:02:32 +0800

    fix database error

--- sts/europaea/files.py
+++ sts/europaea/files.py
@@ -6,7 +6,7 @@ MAP = {
     'KP': ('15B4w3PMbrqCPSKYvTHlSvEEOA-1ioEgL', 'ext'),
     'MS': ('13rnCWi9nwNWH4fXLRvxSsN5nxaWPR3V4', 'pic'),
     'PY': ('1e4zS1wVm5umIUOShaZx0GN2M4fEgV1en', 'mic'),
-    'HQ': ('1J6OD7L0kt_c8eqV8cEN0C0dK2VApGIML', 'aep')
+    'HQ': ('1reTVn0P6iHAcTQV5GtRS3veBj_TmuF9q', 'aep')
 }
 
 def create(sc, proj, row, is_folder):
@@ -18,5 +18,6 @@ def create(sc, proj, row, is_folder):
     else:
         id_ = drive.new(MAP[sc][0], name, type_='doc')
     proj.urls[MAP[sc][1]] = id_
+    proj.save()
     set_hyperlink(sc, row, id_)
     return True
