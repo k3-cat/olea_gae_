52fe07a8f71b4b4ae8dd0390845200dc4c9080b6
Pix-00 <15878537+Pix-00@users.noreply.github.com>
2019-01-08 17:31:38 +0800

    fix np

--- sts/europaea/new.py
+++ sts/europaea/new.py
@@ -6,11 +6,10 @@ def proj(datas):
     path = get_path('LB')
     k = sheets.count_rows(path) + 1
     path.col = 'A:C'
-    data = datas.split('|')
-    path.row = f'{k}:{k+len(data)}'
+    path.row = f'{k}:{k+len(datas)}'
     rows = list()
     projs = list()
-    for item_ in data:
+    for item_ in datas:
         item = item_.split(';')
         projs.append(Project(pid=None, info=(item[0], item[1], item[2])))
         rows.append([f"'{item[0]}", f"'{item[1]}", f"'{projs[-1].pid}", '初始'])
