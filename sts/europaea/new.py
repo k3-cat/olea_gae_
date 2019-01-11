d5c4f802971d8031d9228578dcba5093039087de
Pix-00 <15878537+Pix-00@users.noreply.github.com>
2019-01-11 13:45:38 +0800

    improvement

--- sts/europaea/new.py
+++ sts/europaea/new.py
@@ -13,6 +13,9 @@ def proj(items):
         if ';' not in item_:
             continue
         item = item_.split(';')
+        if item[2] == '':
+            if item[0] != '':
+                item[2] = f'scp-{item[0]}'
         projs.append(Project(pid=None, info=(item[0], item[1], item[2])))
         rows.append([f"'{item[0]}", f"'{item[1]}", f"'{projs[-1].pid}", '初始'])
     sheets.append(path, rows)
