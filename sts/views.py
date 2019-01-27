9b1a2880fe6d21b0ea11e13e6059cc4e698e5956
Pix-00 <15878537+Pix-00@users.noreply.github.com>
2019-01-27 19:58:01 +0800

    improve

--- sts/views.py
+++ sts/views.py
@@ -71,6 +71,8 @@ def edit_staff(request):
     user_info = user.info()
     if request.method == 'GET':
         i = request.GET['i'].split(',')
+        if len(i) > 2 and 'ms' not in user_info['groups'] and 'nimda' not in user_info['groups']:
+            return HttpResponse('不是相应的用户组成员')
         proj = Project(i[0])
         if proj['ssc'] not in SAFE_RANGE[i[1]]:
             return HttpResponseRedirect('/q')
@@ -81,23 +83,26 @@ def edit_staff(request):
         else:
             rows = proj['staff'].detials(i[1])
             empty = ['']*(req-len(rows))
-        return render(request, 'es.html', {
+        parm = {
             'i': {'p': i[0], 's': i[1]},
             'user1': user_info,
-            'joined': (proj[f'staff.{i[1]}'] is not None
-                       and user_info['uid'] in proj[f'staff.{i[1]}']),
-            'req': req,
             'rows': rows,
             'empty': empty,
             'name': proj.name,
             'message': request.GET.get('m', None),
-            'note': ''})
+            'note': ''}
+        if len(i) > 2:
+            return render(request, 'ms.html', parm)
+        else:
+            parm['joined'] = (proj[f'staff.{i[1]}'] is not None
+                              and user_info['uid'] in proj[f'staff.{i[1]}'])
+            return render(request, 'es.html', parm)
     elif request.method == 'POST':
         i = request.POST['i'].split(',')
-        if i[1] not in user_info['groups']:
-            return HttpResponseRedirect(f'/es?i={i[0]},{i[1]}')
         proj = Project(i[0])
         opt = request.POST['opt']
+        if len(i) > 2:
+            uid = request.POST['uid']
         if opt == "F":
             response = proj['staff'].finish_job(i[1], uid)
         elif opt == 'A':
@@ -113,8 +118,7 @@ def edit_staff(request):
         records.update_m_process_info(proj)
         if proj['staff'].get_state(i[1]) == 0:
             PUSH_MAP[i[1]](proj)
-            return HttpResponseRedirect('/q')
-        return HttpResponseRedirect(f'/es?i={i[0]},{i[1]}&m={response}')
+        return HttpResponseRedirect(f'/es?i={",".join(i)}&m={response}')
     return HttpResponseRedirect('/q')
 
 
@@ -137,57 +141,6 @@ def new_projs(request):
         elif type_ in ('G', 'K'):
             append.kp(projs)
         return HttpResponse(True)
-    return HttpResponseRedirect('/q')
-
-def manage_staff(request):
-    uid = request.COOKIES.get('uid', None)
-    if not uid:
-        return HttpResponseRedirect(f'/login?r={request.get_full_path()}')
-    user = User(uid)
-    user_info = user.info()
-    if 'ms' not in user_info['groups'] and 'nimda' not in user_info['groups']:
-        return HttpResponse(False)
-    if request.method == 'GET':
-        i = request.GET['i'].split(',')
-        proj = Project(i[0])
-        if proj['ssc'] not in SAFE_RANGE[i[1]]:
-            return HttpResponseRedirect('/q')
-        req = proj[f'req.{i[1]}']
-        if req is None:
-            rows = []
-            empty = []
-        else:
-            rows = proj['staff'].detials(i[1])
-            empty = ['']*(req-len(rows))
-        return render(request, 'ms.html', {
-            'i': {'p': i[0], 's': i[1]},
-            'user1': user_info,
-            'req': req,
-            'rows': rows,
-            'empty': empty,
-            'name': proj.name,
-            'note': ''})
-    elif request.method == 'POST':
-        i = request.POST['i'].split(',')
-        proj = Project(i[0])
-        opt = request.POST['opt']
-        if opt == "F":
-            proj['staff'].finish_job(i[1], request.POST['uid'])
-        elif opt == 'A':
-            proj['staff'].add_staff(i[1], request.POST['uid'], request.POST['data'])
-        elif opt == 'R':
-            proj['staff'].set_req(i[1], request.POST['data'])
-        elif opt == 'E':
-            proj['staff'].edit_job(i[1], request.POST['uid'], request.POST['data'])
-        elif opt == 'D':
-            proj['staff'].del_staff(i[1], request.POST['uid'])
-        proj.save()
-        records.update_s_state(proj, i[1])
-        records.update_m_process_info(proj)
-        if proj['staff'].get_state(i[1]) == 0:
-            PUSH_MAP[i[1]](proj)
-            return HttpResponseRedirect('/q')
-        return HttpResponseRedirect(f'/ms?i={i[0]},{i[1]}')
     return HttpResponseRedirect('/q')
 
 def back(request):
