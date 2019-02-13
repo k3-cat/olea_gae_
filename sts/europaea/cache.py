d95092869278331012656c5a80a8e7cc0138df2e
Pix-00 <15878537+Pix-00@users.noreply.github.com>
2019-02-13 14:38:06 +0800

    fix

--- sts/europaea/cache.py
+++ sts/europaea/cache.py
@@ -1,47 +1,39 @@
-import time
-
 from .common import get_path
 from .global_value import PATH_MAP
 from .google_io import sheets
 
 
-class PidLineCache:
+def fetch_pid_pos(sc):
+    pid_list = list()
+    path = get_path(sc)
+    path.col = 'C'
+    path.row = '2:'
+    for line in sheets.get_values(path):
+        if not line:
+            pid_list.append(None)
+        else:
+            pid_list.append(line[0])
     pid_map = dict()
-    pid_list = dict()
-    time = {sc: 0 for sc in PATH_MAP}
+    for k, pid in enumerate(pid_list, 2):
+        if pid:
+            pid_map[pid] = k
+    return pid_map
 
-    @classmethod
-    def update_list(cls, sc):
-        cls.time[sc] = time.time()
-        cls.pid_list[sc] = list()
-        path = get_path(sc)
-        path.col = 'C'
-        path.row = '2:'
-        for line in sheets.get_values(path):
-            if not line:
-                cls.pid_list[sc].append(None)
-            else:
-                cls.pid_list[sc].append(line[0])
 
-    @classmethod
-    def update(cls, sc):
-        now = time.time()
-        if sc not in cls.time or now - cls.time[sc] > 900:
-            cls.update_list(sc)
-        cls.pid_map[sc] = dict()
-        for k, pid in enumerate(cls.pid_list[sc], 2):
-            if pid:
-                cls.pid_map[sc][pid] = k
+class PidLineCache:
+    pid_map = {sc: fetch_pid_pos(sc) for sc in PATH_MAP}
 
     @classmethod
     def append(cls, sc, pid):
-        cls.pid_list[sc].append(pid)
-        cls.update(sc)
+        cls.pid_map[sc][pid] = len(cls.pid_map[sc]) + 2
 
     @classmethod
     def delete(cls, sc, pid):
-        cls.pid_list[sc].remove(pid)
-        cls.update(sc)
+        pos = cls.pid_map[sc][pid]
+        del cls.pid_map[sc][pid]
+        for pid_, line_no in cls.pid_map[sc].items():
+            if line_no > pos:
+                cls.pid_map[sc][pid_] -= 1
 
     @classmethod
     def get(cls, sc, pid):
